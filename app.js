0b392bcf131ba9db7984af293afb456c89a09d9a
VinSjo <vincentsjogren@gmail.com>
2021-11-23 17:19:42 +0100

    managed to make it a bit shorter with the use of RegExp.test(), now I'm done!

--- app.js
+++ app.js
@@ -1,55 +1,60 @@
 /*
 TILL DANNIE:
-Hoppas detta inte är alltför svårläst! 
-Gjorde en mycket större lösning först (onödigt stor och invecklad) med classes 
-och modules och grejer, men jag försökte utmana mig själv genom att göra koden
-så kort som möjligt (med vissa undantag för att inte helt förstöra läsbarheten)
-och då även få en bra möjlighet att repetera de array-funktioner vi 
-gått igenom senaste tiden.
+Hoppas detta inte är alltför svårläst!
+
+Jag började den här uppgiften med helt onödigt stor och invecklad lösning,
+med classes och modules och grejer, men kände sen att det vore roligare att
+se hur kort koden skulle kunna bli.
+
+Därför har jag försökt göra koden så kort som möjligt, med prettier-formattering
+och max 80 tecken i bredd.
+Detta gav mig också ett bra tillfälle att öva lite på ett par av de array-metoder
+vi gått igenom senaste tiden
+
+Koden hade såklart kunnat bli ännu kortare men jag kompromissade lite för att 
+försöka upprätthålla en rimlig namngivning på variabler/funktioner och en 
+logisk/funktionell struktur överlag.
 */
-/** Create RegExp that matches characters in a string */
-const getRegX = (chars) => (!!chars ? new RegExp(`[${chars}]`, "ig") : /\0/);
-/** Validate user input, true if input has a length of 1 and is in range a-z */
-const validateInput = (input) => !!input.match(/[a-z]/i) && input.length === 1;
-/** Get matches in a string from a RegExp as an array, empty if no matches */
-const getMatches = (str, regExp) => str.match(regExp) || [];
-/** Determine if all characters in a string matches a RegExp */
-const matchesAll = (str, regExp) => getMatches(str, regExp).join("") === str;
-/** Hangman Play Function */
-const play = () => {
-	const gameWord = [
-		"Programmering",
-		"Stockholm",
-		"Studenter",
-		"Javascript",
-		"Afterwork",
-	][Math.floor(Math.random() * 5)];
-	const player = {
-		lives: 5,
-		guesses: "",
-		hasQuit: false,
-		hasWon: false,
-	};
-	while (player.lives > 0 && !player.hasQuit && !player.hasWon) {
-		const message = `${[...gameWord]
-			.map((char) => (!!char.match(getRegX(player.guesses)) ? char : "_"))
-			.join(" ")}
-		\n\nLives left: ${player.lives}
-		\nGuesses made: ${[...player.guesses]
-			.filter((char) => !char.match(getRegX(gameWord)))
-			.join(", ")}`;
-		let validInput;
-		do {
-			const guess = prompt(message);
-			if ((player.hasQuit = guess === null)) break;
-			if (!(validInput = validateInput(guess))) continue;
-			player.guesses += guess;
-			if (!gameWord.match(getRegX(guess))) player.lives--;
-			else player.hasWon = matchesAll(gameWord, getRegX(player.guesses));
-		} while (!validInput);
-	}
-	if (player.hasQuit) alert("You have canceled the game");
-	else if (player.hasWon) alert("You have won, congratulations!");
-	else alert(`You have lost, the correct word was ${gameWord}`);
+
+/** Determine if variable is a string and in range a to z */
+const aToZ = str => typeof str === "string" && /[a-z]/i.test(str);
+/** Create RegExp that matches characters in a given string */
+const regX = chars => (aToZ(chars) ? new RegExp(`[${chars}]`, "ig") : /\0/);
+/** Determine if all characters in a string matches a RegExp pattern */
+const matchesAll = (str, regExp) => (str.match(regExp) || []).join("") === str;
+
+const word = [
+	"Programmering",
+	"Stockholm",
+	"Studenter",
+	"Javascript",
+	"Afterwork",
+][Math.floor(Math.random() * 5)];
+
+const player = {
+	lives: 5,
+	guesses: "",
+	hasQuit: false,
+	hasWon: false,
 };
-play();
+
+while (player.lives > 0 && !player.hasQuit && !player.hasWon) {
+	const mapper = char => (regX(player.guesses).test(char) ? char : "_");
+	const filterer = char => !regX(char).test(word);
+	const message =
+		`${[...word].map(mapper).join(" ")}\n\nLives left: ${player.lives}` +
+		`\nGuesses made: ${[...player.guesses].filter(filterer).join(", ")}`;
+	let validInput;
+	do {
+		const guess = prompt(message);
+		if ((player.hasQuit = guess === null)) break;
+		if (!(validInput = aToZ(guess) && guess.length === 1)) continue;
+		player.guesses += guess;
+		if (!regX(guess).test(word)) player.lives--;
+		else player.hasWon = matchesAll(word, regX(player.guesses));
+	} while (!validInput);
+}
+
+if (player.hasQuit) alert("You have canceled the game");
+else if (player.hasWon) alert("You have won, congratulations!");
+else alert(`You have lost, the correct word was ${word}`);
